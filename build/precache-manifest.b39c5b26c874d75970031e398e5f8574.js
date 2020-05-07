183826ce525e4368d5257c349b75943cb5a648d3
LordZeF <josefogi@gmail.com>
2020-05-07 10:22:08 -0800

    build

---
+++ build/precache-manifest.b39c5b26c874d75970031e398e5f8574.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f38ed74c8f0db1d427799a66496715f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e066fb0c09c3f81f7d4e",
+    "url": "/static/css/main.be8aeec3.chunk.css"
+  },
+  {
+    "revision": "e8969f9c99c00759d100",
+    "url": "/static/js/2.798058dd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.798058dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e066fb0c09c3f81f7d4e",
+    "url": "/static/js/main.0d3571d1.chunk.js"
+  },
+  {
+    "revision": "82d78d469e740e17ba8b",
+    "url": "/static/js/runtime-main.a7e56ded.js"
+  }
+]);
